9a81cdfa7367b62928e66415215fc82bcd4dd6ce
Usuariofr <chicofasc@gmail.com>
2022-04-04 11:08:29 -0300

    criacao da classe usuario

---
+++ 2-04042022/usuario.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <sting>
+using std::cout;
+using std::cin;
+using std::string;
+
+class Usuario{
+
+    public:
+        string nome;
+        string senha;
+        string email;
+        string nivel;
+
+};
